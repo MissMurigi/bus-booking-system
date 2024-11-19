1863d40aeb22debd7e391b9f0ae627967e736a23
victoriamurigi <vickymurigi001@gmail.com>
2024-11-20 00:28:56 +0300

    hash password

--- backend/models.py
+++ backend/models.py
@@ -43,7 +43,7 @@ class User(db.Model):
         'user_id': self.user_id,
         'username': self.username,
         'email': self.email,
-        'password': self.password, 
+        'password': self.password_hash, 
         'role': self.role,
         'created_at': self.created_at.isoformat() if self.created_at else None,
         'updated_at': self.updated_at.isoformat() if self.updated_at else None,
